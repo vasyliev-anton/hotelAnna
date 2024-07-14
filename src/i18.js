e770dadfb61a734c34ef2be141357eb2eea83b1d
Anton Vasyliev <116097498+vasyliev-anton@users.noreply.github.com>
2024-07-14 11:38:08 +0300

    i18 fixes

--- src/i18.js
+++ src/i18.js
@@ -9,7 +9,7 @@ i18n
     fallbackLng: 'ua',
     lng: 'ua', 
     backend: {
-      loadPath: '/hotelanna/locales/{{lng}}/{{ns}}.json',
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
     },
     debug: true,
     react: {
